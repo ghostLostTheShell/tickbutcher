cd3b24718d0b806d7ae4e0b141878ae5a643502e
ABC <win2233ter23@gmail.com>
2025-09-17 16:12:15 +0800

    添加polygonk线拉取

--- src/tickbutcher/data/put_to_db/__init__.py
+++ src/tickbutcher/data/put_to_db/__init__.py
@@ -1,9 +1,32 @@
 
+from abc import ABC, abstractmethod
+from datetime import datetime, timedelta
+import enum
 import httpx
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional,Tuple
 
 
-class Binance:
+
+class TimeframeType(enum.Enum):
+  sec1 = 0
+  min1 = 1
+  min5 = 2
+  min15 = 3
+  h1 = 4
+  h4 = 5
+  d1 = 6
+  w1 = 7
+  mo1 = 8
+  y1 = 9
+
+
+OHLC = Tuple[int, float, float, float, float, float]
+
+class Exchange(ABC):
+  @abstractmethod
+  async def fetch_ohlcv(self, symbol:str, timeframe:TimeframeType, start_time:datetime, limit:int)->List[OHLC]: ...
+
+class Binance(Exchange):
   """
   symbol 规则 BTC/USDT 表示现货, BTC/USDT@p
   """
@@ -49,7 +72,7 @@ class Binance:
     return data
     
   
-  async def fetch_ohlcv(self, symbol:str, timeframe:str, since:int, limit:int=500):
+  async def fetch_ohlcv(self, symbol:str, timeframe:str, start_time:int, end_time:int, limit:int=500 ): # type: ignore
     url = f"{self.spot_base_url}/klines"
     if self.is_perpetual_swap(symbol):
       url = f"{self.ps_base_url}/klines"
@@ -62,7 +85,7 @@ class Binance:
     params = {
         "symbol": symbol,
         "interval": timeframe,
-        "startTime": since,
+        "startTime": start_time,
         # "endTime": end_time,
         "limit": limit
     }
@@ -70,8 +93,101 @@ class Binance:
     if response.status_code != 200:
         raise Exception(f"Error fetching data: {response.text}")
     data = response.json()
-    # 将字符数据转换为浮点数
-    # data = [[int(item[0]), float(item[1]), float(item[2]), float(item[3]), float(item[4]), float(item[5])] for item in data]
-    # [ timestamp, open, high, low, close, volume ]
-    
     return data
+  
+
+class PolygonEx(Exchange):
+  """
+  拉取股票数据
+  """
+
+  def __init__(self, all_stock: str, candle_url: str, api_key: Optional[str]=None):
+    self.all_stock = all_stock
+    self.candle_url = candle_url
+    self.session = httpx.AsyncClient()
+    self.api_key = api_key
+
+
+
+  async def fetch_ohlcv(self, symbol:str, timeframe:TimeframeType, start_time:datetime, limit:int):
+
+    symbol = symbol.split("/")[0]  # 去掉股票代码中的市场后缀，如 "AAPL/USD" -> "AAPL"
+    end_time:datetime
+
+    _timeframe:str
+    _s:int= int(start_time.timestamp() * 1000)
+    _e:int=0
+    match timeframe:
+      case TimeframeType.sec1:
+        _timeframe = "1/second"
+        end_time = start_time + timedelta(seconds=limit)
+        
+      case TimeframeType.min1:
+        _timeframe = "1/minute"
+        end_time = start_time + timedelta(minutes=limit)
+      case TimeframeType.min5:
+        _timeframe = "5/minute"
+        end_time = start_time + timedelta(minutes=5*limit)
+      case TimeframeType.min15:
+        _timeframe = "15/minute"
+        end_time = start_time + timedelta(minutes=15*limit)
+      case TimeframeType.h1:
+        _timeframe = "1/hour"
+        end_time = start_time + timedelta(hours=limit)
+      case TimeframeType.h4:
+        _timeframe = "4/hour"
+        end_time = start_time + timedelta(hours=4*limit)
+      case TimeframeType.d1:
+        _timeframe = "1/day"
+        end_time = start_time + timedelta(days=limit)
+      case TimeframeType.w1:
+        raise  Exception("mo1Not implemented yet")
+        # _timeframe = "1Week"
+        # end_time = start_time + timedelta(weeks=limit)
+      case TimeframeType.mo1:
+        raise  Exception("mo1Not implemented yet")
+        # _timeframe = "1Month"
+        # end_time = start_time + timedelta(days=30*limit)
+      case TimeframeType.y1:
+        raise  Exception("y1Not implemented yet")
+        _timeframe = "1Year"
+      case _:
+        raise ValueError(f"Invalid timeframe: {timeframe}") 
+    _e = int(end_time.timestamp() * 1000)
+    url = f"{self.candle_url}aggs/ticker/{symbol}/range/{_timeframe}/{_s}/{_e}"
+    params = {
+        "adjusted": "true",
+        "sort": "asc",
+        "apiKey": self.api_key,
+        "limit": limit
+    }
+    response = await self.session.get(url, params=params)
+    if response.status_code != 200:
+        raise Exception(f"Error fetching data: {response.text}")
+    data = response.json()
+
+
+    data = [(item['t'], item['o'], item['h'], item['l'], item['c'], item['v']) for item in data.get('results', [])]
+
+    return data
+  
+  async def fetch_all_stock(self):
+    print(f"Fetching all stock data from {self.all_stock}")
+    url = f"{self.all_stock}tickers"
+    params = {
+        "market": "stocks",
+        "active": "true",
+        "sort": "ticker",
+        "order": "asc",
+        "limit": 1000,
+        "apiKey": self.api_key
+    }
+    response = await self.session.get(url, params=params)
+    if response.status_code != 200:
+        raise Exception(f"Error fetching data: {response.text}")
+    data = response.json()
+
+
+    return data
+
+
--- src/tickbutcher/data/put_to_db/__main__.py
+++ src/tickbutcher/data/put_to_db/__main__.py
@@ -1,18 +1,15 @@
- # type: ignore
 import argparse
 import asyncio
 import os
-from sqlalchemy import *
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.orm import *
-from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy import Integer, Double, select
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncAttrs, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from datetime import datetime, timedelta
+from typing import cast, Optional
+from tickbutcher.data.put_to_db import Exchange, TimeframeType
 
 
-DATA_SOURCE_URL = os.environ.get("DATA_SOURCE_URL") 
-if DATA_SOURCE_URL is None:
-  DATA_SOURCE_URL = "sqlite+aiosqlite:///./app.db"
-
+DATA_SOURCE_URL = os.environ.get("DATA_SOURCE_URL", "sqlite+aiosqlite:///./tmp/app.db")
 
 async_engine = create_async_engine(DATA_SOURCE_URL)
 
@@ -24,121 +21,124 @@ parser.add_argument('--timeframe', type=str, help='时间框架' , required=True
 parser.add_argument('--start_date', type=str, help='开始时间' , required=True)
 parser.add_argument('--end_date', type=str, help='结束时间' , required=True)
 parser.add_argument('--exchange', type=str, help='交易所名称' , default='binance')
-parser.add_argument('--limit', type=int, help='单次获取数量' , default=1000)
+parser.add_argument('--limit', type=int, help='单次获取数量' , default=10)
 parser.add_argument('--overwrite', type=bool, help='是否覆盖已存在数据', default=False)
+parser.add_argument('--api_key', type=str, help='api请求认证', required=False)
+
 
 # 解析命令行参数
 args = parser.parse_args()
 
-code = args.code #"BTC/USDT" #"SOL/USDT" #
-timeframe = args.timeframe #'1h' #'15m' # 分钟m h小时 d天 M月
-start_date =  args.start_date #'2020-01-01T00:00:00Z'
-end_date = args.end_date #'2024-12-01T00:00:00Z'
-exchange_name = args.exchange #'binance' # 'okx' #'binance'
-limit = args.limit #binance limit max 1000
-overwrite = args.overwrite #False
+# args.code         "BTC/USDT" #"SOL/USDT"
+# args.timeframe    '1h' #'15m' # 分钟m h小时 d天 M月
+# args.start_date   '2020-01-01T00:00:00Z'
+# args.end_date     '2024-12-01T00:00:00Z'
+# exchange_name     'binance' # 'okx' #'binance'
+# # type: ignore    #binance limit max 1000
+# type: ignore      #False
+
+code = cast(str, args.code)
+timeframe:str = cast(str, args.timeframe)
+start_date:str = cast(str, args.start_date)
+end_date:str = cast(str, args.end_date) 
+exchange_name:str = cast(str, args.exchange) 
+limit:int = cast(int, args.limit)
+overwrite:bool = args.overwrite
+api_key:Optional[str] = cast(Optional[str], args.api_key)
+
+
 
 class BaseEntity(AsyncAttrs, DeclarativeBase):
-    pass
+  pass
 
 
 class DataEntity(BaseEntity):
-    __tablename__                   = f"t_{code}_{timeframe}"
-    id: Mapped[int]                 = mapped_column(Integer(), primary_key=True, autoincrement=True)
-    timestamp:Mapped[int]           = mapped_column(Integer(), unique=True)
-    open:Mapped[float]              = mapped_column(Double())
-    high:Mapped[float]              = mapped_column(Double())
-    low:Mapped[float]               = mapped_column(Double())
-    close:Mapped[float]             = mapped_column(Double())
-    volume:Mapped[float]            = mapped_column(Double())
+  __tablename__                   = f"t_{code}_{timeframe}"
+  id: Mapped[int]                 = mapped_column(Integer(), primary_key=True, autoincrement=True)
+  timestamp:Mapped[int]           = mapped_column(Integer(), unique=True)
+  open:Mapped[float]              = mapped_column(Double())
+  high:Mapped[float]              = mapped_column(Double())
+  low:Mapped[float]               = mapped_column(Double())
+  close:Mapped[float]             = mapped_column(Double())
+  volume:Mapped[float]            = mapped_column(Double())
 
 # 获取数据的函数
-async def record_data(symbol, exchange, timeframe='1h', start_date='2022-01-01T00:00:00Z', end_date='2023-01-01T00:00:00Z'):
+async def record_data(symbol:str, 
+                      exchange:Exchange, 
+                      start_date:datetime, 
+                      end_date:datetime,
+                      timeframe:TimeframeType,
+                      limit:int)->None:
     # 将时间转换为时间戳
-    since_timestamp = exchange.parse8601(start_date)
-    end_timestamp = exchange.parse8601(end_date)
-    
-    async_session = async_sessionmaker(async_engine, expire_on_commit=False)
-    
-    async with async_session() as session:
-      if not overwrite:
-        sql = select(DataEntity).order_by(DataEntity.timestamp.desc()).limit(1)
 
-        data = await session.scalar(sql)
-        if data is not None:
-            since_timestamp = data.timestamp
+  async_session = async_sessionmaker(async_engine, expire_on_commit=False)
+  
+  async with async_session() as session:
+    if not overwrite:
+      sql = select(DataEntity).order_by(DataEntity.timestamp.desc()).limit(1)
 
-        ### 根据timeframe调整加上偏移量 1s|1m|15m
-        if timeframe == '1s':
-            since_timestamp += 1000
-        elif timeframe == '1m':
-            since_timestamp += 60000
-        elif timeframe == '15m':
-            since_timestamp += 900000
-        elif timeframe == '1h':
-            since_timestamp += 3600000
-        elif timeframe == '1d':
-            since_timestamp += 86400000
+      data = await session.scalar(sql)
+      if data is not None:
+          since_timestamp = data.timestamp
+          start_date = datetime.fromtimestamp(since_timestamp, tz=start_date.tzinfo)
 
-      while True:
-        # 获取K线数据
+    while True:
 
-        # 多线程获取数据，存在问题
-        #   data = []
-        #   thread_pool = []
-        #   execution_count = 0
-        #   start_time = time.time()
+      data = await exchange.fetch_ohlcv(symbol, timeframe, start_time=start_date, limit=limit)
 
-        #   while time.time() - start_time < 60 and execution_count < 3000:
-        #       thread = Thread(target=lambda: data.append(exchange.fetch_ohlcv(symbol, timeframe, since=since_timestamp, limit=limit)))
-        #       thread.start()
-        #       thread_pool.append(thread)
-
-        #   for thread in thread_pool:
-        #       thread.join()
-
-        data = exchange.fetch_ohlcv(symbol, timeframe, since=since_timestamp, limit=limit)
-
-        if len(data) == 0:
-            break  # 如果没有数据返回，退出循环
-        
-        for item in data:
-        
-        # select(exists().where(DataEntity.timestamp==item[0]))
-
-            entity = DataEntity(timestamp=item[0], open=item[1], high=item[2],  low=item[3], close=item[4], volume=item[5])
-            session.add(entity)
-
-        
-        since_timestamp = data[-1][0] + 1  # 1毫秒的偏移，防止获取到重复的数据
-        if since_timestamp > end_timestamp:
+      if len(data) == 0:
+        break  # 如果没有数据返回，退出循环
+      
+      for item in data:
+        if item[0] > int(end_date.timestamp() * 1000):
             break
         
-        print(f"Fetched {len(data)} records, next since: {since_timestamp}")
-        await session.flush()
-        await session.commit()
-        
+        entity = DataEntity(timestamp=item[0], open=item[1], high=item[2],  low=item[3], close=item[4], volume=item[5])
+        session.add(entity)
+      
+      start_date = datetime.fromtimestamp(data[-1][0] / 1000, tz=start_date.tzinfo) + timedelta(milliseconds=1)
+      if start_date >= end_date:
+        break
+      print(f"Fetched {len(data)} records, next since: {start_date}")
       await session.flush()
       await session.commit()
+      
+    await session.flush()
+    await session.commit()
 
 async def main():
-  
+  global start_date, end_date, limit
   async with async_engine.begin() as conn:
         # 创建表
         await conn.run_sync(DataEntity.metadata.create_all)
-  
-  # 初始化ccxt连接
+
   if exchange_name == 'binance':
       from . import Binance
       exchange = Binance()
+  elif exchange_name == 'polygon':
+      from . import PolygonEx
+      exchange = PolygonEx(all_stock = "https://api.polygon.io/v3/reference/", candle_url = "https://api.polygon.io/v2/",api_key = api_key)
+    
   else:
     raise Exception(f"不支持的交易所: {exchange_name}")
+
   
-  await record_data(code, exchange, timeframe=timeframe, start_date=start_date, end_date=end_date)
+
+  if start_date.endswith("Z"): 
+      start_date = start_date.replace("Z", "+00:00")
+  if end_date.endswith("Z"):
+      end_date = end_date.replace("Z", "+00:00")
+
+  await record_data(code, 
+                    exchange, 
+                    timeframe=TimeframeType[timeframe], 
+                    start_date=datetime.fromisoformat(start_date), 
+                    end_date=datetime.fromisoformat(end_date),
+                    limit=limit
+                    )
 
 
 
 if __name__ == "__main__":
-  
   asyncio.run(main())
   
